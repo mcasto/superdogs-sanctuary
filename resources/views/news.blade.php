b2b4247771d90c4f5009d56ef22c2b7c905c4477
Mike Casto <castoware@gmail.com>
2026-01-30 21:53:56 -0500

    Added download for january 2026 update PDF

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -186,6 +186,18 @@
     }
 </style>
 
+<div class='q-py-md'>
+    <div class='flex justify-center'>
+        <div class='text-h6'>
+            View/Download January 2026 Update <a
+                href="https://firebasestorage.googleapis.com/v0/b/gala-staff.firebasestorage.app/o/campaign%2F202601-faan.pdf?alt=media&token=5f287f46-ab9f-4fc6-b20e-50e2421aea76">PDF</a>
+            {{-- ggignore --}}
+        </div>
+    </div>
+</div>
+
+<hr />
+
 <div class='flex justify-center'>
     <div>
         <div class="q-pa-md bg-green q-mb-xl">

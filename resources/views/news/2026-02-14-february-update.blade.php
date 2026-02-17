61012a255eec92172d7cda2ec825697d43b5a6e3
Mike Casto <castoware@gmail.com>
2026-02-16 19:19:04 -0500

    2026-02-16 updates

---
+++ resources/views/news/2026-02-14-february-update.blade.php
@@ -0,0 +1,10 @@
+{{-- title: February 2026 Update | date: 2026-02-14 --}}
+<div class='q-py-md'>
+    <div class='flex justify-center'>
+        <div class='text-h6'>
+            View/Download February 2026 Valentine's Update <a
+                href="/storage/documents/202602-faan-valentines-update.pdf">PDF</a>
+            | <a href="/storage/documents/202602-faan-valentines-update-es.pdf">PDF (Español)</a>
+        </div>
+    </div>
+</div>
---
+++ resources/views/news/2026-02-15-gringo-post-best-animal-rescue.blade.php
@@ -0,0 +1,10 @@
+{{-- title: Thank You Gringo Post Readers! | date: 2026-02-15 --}}
+<div class='q-py-md'>
+    <div class='flex justify-center'>
+        <div style="max-width: 600px; text-align: center;">
+            <img src="/storage/images/thank-you-gringo-post-readers.jpg"
+                 alt="FAAN - Best Animal Rescue 2026 - Best of GringoPost Award - Thank You Gringo Post Readers"
+                 style="width: 100%; height: auto; border-radius: 8px; box-shadow: 0 4px 20px rgba(0, 0, 0, 0.1);">
+        </div>
+    </div>
+</div>
